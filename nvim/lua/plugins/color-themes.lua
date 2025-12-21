65a54cfddd7af973bed8b1fae53b39d387cd2c58
wanghl1989 <wanghl1989@gmail.com>
2025-12-21 15:25:56 +0800

    add: eza config

--- nvim/lua/plugins/color-themes.lua
+++ nvim/lua/plugins/color-themes.lua
@@ -14,19 +14,19 @@ return {
       saturation = 1,
       terminal_color = false,
       borderless_pickers = false,
-    },
 
-    highlights = {
-      Comment = { fg = "#898989", bg = "NONE", italic = true },
-    },
+      highlights = {
+        Comment = { fg = "#898989", bg = "NONE", italic = true },
+      },
 
-    colors = {
-      bg = "#0B0C16",
-      fg = "#ddf7ff",
-      green = "#4fe88f",
-      blue = "#829dd4",
-      magenta = "#86a7df",
-      cyan = "#7cf8f7",
+      colors = {
+        bg = "#0B0C16",
+        fg = "#ddf7ff",
+        green = "#50f872",
+        blue = "#829dd4",
+        magenta = "#ff5ef1",
+        cyan = "#7cf8f7",
+      },
     },
   },
   {
