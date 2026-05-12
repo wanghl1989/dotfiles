0406042a90f9e1e3679bc58b008fdc83e5311203
wanghl <whelouch@gmail.com>
2026-05-12 09:20:00 +0800

    remove: old linux software config

--- linux/niri/script/load_waybar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css &
--- linux/niri/script/reload_waybar.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-killall waybar
-
-sleep 1
-
-waybar -c ~/.config/waybar/config.jsonc -s ~/.config/waybar/style.css
