ff4d60ce0365032a46eddecb1aa7b4b7785ad2af
wanghl1989 <whlelouch@gmail.com>
2026-07-20 09:53:38 +0800

    update: kitty verticle bar

--- config/kitty/tab_bar.py
+++ config/kitty/tab_bar.py
@@ -1,9 +1,8 @@
 # pyright: reportMissingImports=false
 import os
-from datetime import datetime
 
 from kitty.boss import get_boss
-from kitty.fast_data_types import Screen, add_timer, get_options
+from kitty.fast_data_types import Screen, get_options
 from kitty.tab_bar import (
     DrawData,
     ExtraData,
@@ -62,12 +61,11 @@ def _draw_icon(screen: Screen, index: int) -> int:
     screen.draw(ICON)
     screen.cursor.fg = icon_fg
     screen.cursor.bg = icon_bg
-    screen.draw(SEPARATOR_LEFT)
+    screen.draw("\n")
     screen.cursor.fg = 0
-    screen.cursor.x = len(ICON) + len(SEPARATOR_LEFT)
+    # screen.cursor.x = len(ICON)
 
     return screen.cursor.x
-
 
 
 def _draw_left_status(
@@ -91,9 +89,9 @@ def _draw_left_status(
     if trailing_spaces:
         screen.draw(" " * trailing_spaces)
     end = screen.cursor.x
-    if not is_last:
-        screen.cursor.bg = bar_bg
-        screen.draw(SEPARATOR_DOT)
+    # if not is_last:
+    #     screen.cursor.bg = bar_bg
+    #     screen.draw(SEPARATOR_DOT)
     screen.cursor.bg = bar_bg
     return end
 
@@ -108,7 +106,7 @@ def _draw_right_status(screen: Screen, is_last: bool, cells: list) -> int:
         # screen.cursor.bg = bar_bg if i == 0 else cells[i-1][1]
         # screen.cursor.fg = color_fg
         # screen.draw(SEPARATOR_RIGHT)
-        
+
         screen.cursor.fg = bar_bg
         screen.cursor.bg = color_fg
         screen.draw(status)
@@ -139,21 +137,19 @@ def draw_tab(
     is_last: bool,
     extra_data: ExtraData,
 ) -> int:
-    global timer_id
-    global right_status_length
-    if timer_id is None:
-        timer_id = add_timer(_redraw_tab_bar, REFRESH_TIME, True)
-    now = datetime.now()
+    # global timer_id
+    # global right_status_length
+    # if timer_id is None:
+    #     timer_id = add_timer(_redraw_tab_bar, REFRESH_TIME, True)
+    # now = datetime.now()
     # clock = now.strftime(CLOCK)
-    date = now.strftime(DATE)
+    # date = now.strftime(DATE)
     cells = []
-
     # cells.append((clock, clock_fg, clock_bg))
-    cells.append((date, date_fg, date_bg))
+    # cells.append((date, date_fg, date_bg))
+    # right_status_length = _cell_length(cells)
 
-    right_status_length = _cell_length(cells)
-
-    _draw_icon(screen, index)
+    # _draw_icon(screen, index)
     _draw_left_status(
         draw_data,
         screen,
@@ -164,9 +160,9 @@ def draw_tab(
         is_last,
         extra_data,
     )
-    _draw_right_status(
-        screen,
-        is_last,
-        cells,
-    )
+    # _draw_right_status(
+    #     screen,
+    #     is_last,
+    #     cells,
+    # )
     return screen.cursor.x
