52214e2d873dff70f344ddb80b1235e9ab220294
wanghl <whelouch@gmail.com>
2026-08-11 11:16:28 +0800

    add: sesh config

--- script/install_config.sh
+++ script/install_config.sh
@@ -34,7 +34,7 @@ os_kernel=$(uname -s)
 
 # 在 ~/.config中的配置
 # macos linux通用
-ITEMS=("zed" "nvim" "kitty" "uv" "ruff" "starship.toml" "tmux" "ghostty" "lazygit" "bat" "yazi" "btop" "lazydocker" "herdr" "eza" "newsboat" "pip" "fastfetch" "dict" "fish" "alacritty")
+ITEMS=("zed" "nvim" "kitty" "uv" "ruff" "starship.toml" "tmux" "ghostty" "lazygit" "bat" "yazi" "btop" "lazydocker" "herdr" "eza" "newsboat" "pip" "fastfetch" "dict" "fish" "alacritty" "sesh")
 BASE_ROOT=$(dirname $(dirname "$SCRIPT_PATH"))
 
 DEST_DIR="$HOME/.config"
