0f116bd71250aac612339c4ac93d637f2b2c3cbf
wanghl1989 <whlelouch@gmail.com>
2026-09-14 13:55:45 +0800

    fix: yanky config for error bug

--- config/nvim/lua/plugins/yanky.lua
+++ config/nvim/lua/plugins/yanky.lua
@@ -4,7 +4,7 @@ vim.pack.add({
 
 require("yanky").setup({
 	system_clipboard = {
-		sync_with_ring = not vim.env.SSH_CONNECTION,
+		sync_with_ring = false,
 		clipboard_register = "+",
 	},
 	highlight = {
